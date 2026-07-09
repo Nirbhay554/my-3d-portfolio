e6e9f7e335ec293efd005c1ce0f110084ab090f1
Nirbhay Singh <nirbhaysingh4585@gmail.com>
2026-07-09 07:26:39 +0530

    Personalize portfolio with my credentials

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,16 +43,16 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
+          NS
         </a>
         <a
-          href="https://www.linkedin.com/in/akashrmalhotra/"
+          href="https://www.linkedin.com/in/nirbhay-singh-79b60132a"
           className="navbar-connect"
           data-cursor="disable"
           target="_blank"
           rel="noreferrer"
         >
-          linkedin.com/in/akashrmalhotra
+          linkedin.com/in/nirbhay-singh-79b60132a
         </a>
         <ul>
           <li>
