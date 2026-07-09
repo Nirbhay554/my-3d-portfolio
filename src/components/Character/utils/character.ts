e0354dd3da2cb950fbefadc723e4108597fd856b
Nirbhay Singh <nirbhaysingh4585@gmail.com>
2026-07-09 09:02:17 +0530

    Revert cap removal

--- src/components/Character/utils/character.ts
+++ src/components/Character/utils/character.ts
@@ -29,9 +29,6 @@ const setCharacter = (
             character = gltf.scene;
             await renderer.compileAsync(character, camera, scene);
             character.traverse((child: any) => {
-              if (child.name.toLowerCase().includes("cap") || child.name.toLowerCase().includes("hat")) {
-                child.visible = false;
-              }
               if (child.isMesh) {
                 const mesh = child as THREE.Mesh;
 
